44a5a16b19ce27ed02b363ff543b5099b60e3803
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2021-10-16 18:00:20 +0200

    updated tests for multiple value cases

--- src/test/java/de/vinogradoff/testdatabroker/TestDataControllerTest.java
+++ src/test/java/de/vinogradoff/testdatabroker/TestDataControllerTest.java
@@ -23,6 +23,15 @@ public class TestDataControllerTest {
         assertThat(controller.repo.get("dict").get("key")).isEqualTo(List.of("value"));
     }
 
+    @Test
+    void shouldPutMultipleTestDataInRepo() {
+        // act
+        controller.saveData("dict", "key", "value");
+        controller.saveData("dict", "key", "value2");
+        // assert
+        assertThat(controller.repo).containsKey("dict");
+        assertThat(controller.repo.get("dict").get("key")).isEqualTo(List.of("value", "value2"));
+    }
 
     @Test
     void shouldGetDataFromRepoMultipleTimes() {
@@ -50,5 +59,21 @@ public class TestDataControllerTest {
         // assert
         assertThat(value1).isEqualTo("value2");
         assertThat(value2).isNull();
+    }
+
+    @Test
+    void shouldClaimDataFromRepoUntilNoDataLeft() {
+        // arrange
+        controller.repo.put("dict", new HashMap<>(Map.of("key2", new ArrayList<>(List.of("value1", "value2")))));
+
+        // act
+        var value1 = controller.claimData("dict", "key2");
+        var value2 = controller.claimData("dict", "key2");
+        var value3 = controller.readData("dict", "key2");
+
+        // assert
+        assertThat(value1).isEqualTo("value1");
+        assertThat(value2).isEqualTo("value2");
+        assertThat(value3).isNull();
     }
 }
