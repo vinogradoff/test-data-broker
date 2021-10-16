d5e5417329487b6f388dd80075045827998335a2
Alexei Vinogradov <6568032+vinogradoff@users.noreply.github.com>
2021-10-16 16:09:47 +0200

    refactored tests

--- src/test/java/de/vinogradoff/testdatabroker/TestDataControllerTest.java
+++ src/test/java/de/vinogradoff/testdatabroker/TestDataControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -14,23 +16,39 @@ public class TestDataControllerTest {
 
     @Test
     void shouldPutDataInRepo() {
+        // act
         controller.saveData("dict", "key", "value");
+        // assert
         assertThat(controller.repo).containsKey("dict");
         assertThat(controller.repo.get("dict").get("key")).isEqualTo("value");
     }
 
 
     @Test
-    void shouldGetDataFromRepo() {
-        controller.saveData("dict", "key1", "value1");
-        assertThat(controller.readData("dict", "key1")).isEqualTo("value1");
-        assertThat(controller.readData("dict", "key1")).isEqualTo("value1");
+    void shouldGetDataFromRepoMultipleTimes() {
+        // arrange
+        controller.repo.put("dict", new HashMap<>(Map.of("key1", "value1")));
+
+        // act
+        var value1 = controller.readData("dict", "key1");
+        var value2 = controller.readData("dict", "key1");
+
+        // assert
+        assertThat(value1).isEqualTo("value1");
+        assertThat(value2).isEqualTo("value1");
     }
 
     @Test
-    void shouldClaimDataFromRepo() {
-        controller.saveData("dict", "key2", "value2");
-        assertThat(controller.claimData("dict", "key2")).isEqualTo("value2");
-        assertThat(controller.readData("dict", "key2")).isEqualTo(null);
+    void shouldClaimDataFromRepoJustOnce() {
+        // arrange
+        controller.repo.put("dict", new HashMap<>(Map.of("key2", "value2")));
+
+        // act
+        var value1 = controller.claimData("dict", "key2");
+        var value2 = controller.readData("dict", "key2");
+
+        // assert
+        assertThat(value1).isEqualTo("value2");
+        assertThat(value2).isNull();
     }
 }
